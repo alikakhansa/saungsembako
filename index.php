2a100755bc6e8edba8d0521b51f844824bbdb090
Alika <alikakf19@gmail.com>
2023-06-15 18:26:08 +0700

    landing page

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Saung Sembako</title>
+      <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+   </head>
+   <body>
+      <header>
+         <div class="logo">
+            <img src="assets/images/logosaung.png" alt="Logo" height="50px" width="200px;">
+         </div>
+      </header>
+      <section>
+         <div class="image-container">
+            <img src="assets/images/background.jpg" alt="Gambar" height="750px" width="1349px;">
+            <div class="overlay">
+               <h2 style="font-size: 50px; color: #8bc7ad">Warung Sembako Online</h2>
+               <p style="margin-top: 30px; margin-bottom: 10px;">Saung Sembako is a social enterprise that focuses on providing groceries that are cheaper than market prices for the community. <br>Saung Sembako was established with the main objective of helping overcome the problem of hunger and social tension in the surrounding community.</p>
+               <div class="button">
+                  <button class="hover-button">Login</button>
+               </div>
+            </div>
+         </div>
+      </section>
+      <h1 style="font-size: 40px; text-align:center; margin-top: 50px; margin-bottom: 50px;">what are you selling?</h1>
+      <div class="section-grid">
+         <div class="grid-container">
+            <div class="grid-item">
+               <img src="assets/images/tepung.png" alt="Foto 1">
+               <h3>Tepung</h3>
+               <p>Flour is a solid particle in the form of fine grains which is usually used for research, household and industrial raw materials. Flour can come from vegetable materials, for example wheat flour from wheat, tapioca from cassava, cornstarch from corn, or animal origin, for example bone and fish meal.</p>
+            </div>
+            <div class="grid-item">
+               <img src="assets/images/minyak.png" alt="Foto 2">
+               <h3>Minyak</h3>
+               <p>Oil is a general term for all organic liquids that are insoluble/miscible in water but soluble in organic solvents that function as heat conductors, savory flavor enhancers, and additive caloric value of foodstuffs.</p>
+            </div>
+            <div class="grid-item">
+               <img src="assets/images/gula.png" alt="Foto 3">
+               <h3>Gula</h3>
+               <p>Sugar is a common sweetener consumed by society. Sugar is commonly used as a sweetener in food and beverages, in the food sector, apart from being a sweetener, sugar is also used as a stabilizer and preservative. Sugar is a simple carbohydrate that is generally produced from sugarcane.</p>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
